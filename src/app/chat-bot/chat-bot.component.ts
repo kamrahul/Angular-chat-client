09f8c6cf6a64572fb9db8b973b5b5e7f93b25da2
kamrahul <hi2rahulk@gmail.com>
2021-03-06 23:13:26 +0530

    Client events updated
    Client events updated

--- src/app/chat-bot/chat-bot.component.ts
+++ src/app/chat-bot/chat-bot.component.ts
@@ -16,23 +16,27 @@ export class ChatBotComponent implements OnInit {
   chatText: string;
   message_stack :Array<ChatBubble> =[]
   closeResult: string="";
+  usertype='';
 
 
  
   
-  constructor( private _socketService: SocketService,private modalService: NgbModal
+  constructor( public _socketService: SocketService,private modalService: NgbModal
    
 
   ) { 
 
      this.chatText = '';
+     this._socketService.chatClientId='123';
 
   }
 
   ngOnInit(): void {
 
-    this._socketService.socket.on('my_response', (data: any) => {
-      console.log("DAta received",data.data);
+   
+
+    this._socketService.socket.on('agent_connected', (data: any) => {
+      //console.log("DAta received",data.data);
       //alert(data)
       //this.data = data;
       let chatMessage = new  ChatBubble(data.data,"chat-bubble me");
@@ -41,17 +45,110 @@ export class ChatBotComponent implements OnInit {
     });
 
     // Used when users asks for support
-    this._socketService.socket.on('provide_support', (data: any) => {
+    this._socketService.socket.on('client_connected', (data: any) => {
       console.log("DAta received",data.data);
+     //alert(data)
       // Open pop up if the user is agent
-      this.modalService.open(SupportDialogComponent);
+      //this.modalService.open(SupportDialogComponent);
+      let chatMessage = new  ChatBubble(data.data,"chat-bubble me");
+      this.message_stack.push(chatMessage);
+      this._socketService.chatClientId=data.client_id
+      
+    });
+
+    this._socketService.socket.on('provide_support_all_agents', (data: any) => {
+      console.log("DAta received",data.data);
+      //alert('provide support')
+      // Open pop up if the user is agent
+      //this.modalService.open(SupportDialogComponent);
+
+      let chatMessage = new  ChatBubble("Modal pop up on agents side to confirm the request","chat-bubble me");
+      this.message_stack.push(chatMessage);
+
+      chatMessage = new  ChatBubble("If Yes. Client will get confirmation","chat-bubble me");
+      this.message_stack.push(chatMessage);
+
+
+      this._socketService.socket.emit('support_confirmed', {data: ' I am John , How can I help you ?','client_id':this._socketService.chatClientId});
+      this._socketService.conversation_mode='private';
+      chatMessage = new  ChatBubble(data.data,"chat-bubble me");
+      this.message_stack.push(chatMessage);
+      
+    });
+
+
+    this._socketService.socket.on('setup_private_conversation', (data: any) => {
+      console.log("DAta received",data.data);
+      //alert('provide support')
+      // Open pop up if the user is agent
+      //this.modalService.open(SupportDialogComponent);
+      let chatMessage = new  ChatBubble("Support Confirmed.","chat-bubble me");
+      this.message_stack.push(chatMessage);
+      this._socketService.conversation_mode='private'
+      
+    });
+
+    
+    this._socketService.socket.on('private_conversation', (data: any) => {
+      console.log("DAta received",data.data);
+      //alert('provide support')
+      // Open pop up if the user is agent
+      //this.modalService.open(SupportDialogComponent);
       let chatMessage = new  ChatBubble(data.data,"chat-bubble me");
       this.message_stack.push(chatMessage);
       
     });
+
+    this._socketService.socket.on('leave_private_conversation', (data: any) => {
+      //console.log("DAta received",data.data);
+      //alert('provide support')
+      // Open pop up if the user is agent
+      //this.modalService.open(SupportDialogComponent);
+      //alert('Hey')
+      let chatMessage = new  ChatBubble('Your conversation has been ended.',"chat-bubble me");
+      this.message_stack.push(chatMessage);
+
+      if(this._socketService.userType=='client'){
+        this._socketService.connectClient(this._socketService.chatClientId)
+      }
+      else{
+        //this._socketService.chatClientId=''
+        this._socketService.connectAgent()
+
+      }
+
+      this._socketService.conversation_mode='normal'
+      
+    });
+
+
+
   
   }
 
+
+  clickAgent(){
+
+    this._socketService.connectAgent()
+
+
+  }
+
+  clickClient(){
+
+    this._socketService.connectClient(this._socketService.chatClientId)
+
+
+  }
+
+  clickDisconnect(){
+
+    this._socketService.disconnectClient(this._socketService.chatClientId);
+
+
+
+
+  }
   openModal() {
 
     this.modalService.open(SupportDialogComponent);
@@ -85,14 +182,23 @@ export class ChatBotComponent implements OnInit {
     console.log(this.chatText)
     let chatMessage = new  ChatBubble(this.chatText,"chat-bubble you");
     this.message_stack.push(chatMessage);
+    console.log(this._socketService.conversation_mode)
+    console.log(this._socketService.userType)
+
+     if(this._socketService.conversation_mode=='private'){
+
+      this._socketService.socket.emit('private_conversation', {data: this.chatText,'client_id':this._socketService.chatClientId});
+    }
 
     // if its client user || NOT for agents
-    if(this._socketService.useType = 'client'){
-      alert(this._socketService.useType)
-      this._socketService.socket.emit('send_support', {data: ' I need support','client_id':'123'});
+    if(this._socketService.userType == 'client'){
+      //alert(this._socketService.userType)
+      this._socketService.getAgentSupport(this.chatText)
+      //this._socketService.socket.emit('send_support', {data: ' I need support','client_id':'123'});
       //let chatMessage = new  ChatBubble("Please wait while we connect to an agent","chat-bubble you");
     //this.message_stack.push(chatMessage);
     }
+   
   
 
     //this._socketService.socket.emit('my_broadcast_event', {data: 'Broadcast message SocketServer...'});
--- src/app/service/socket.service.ts
+++ src/app/service/socket.service.ts
@@ -6,25 +6,51 @@ declare var io: any ;
 })
 export class SocketService {
   public socket:any;
-  useType: any= 'agent';
+  userType: any= 'agent';
+  conversation_mode: string ='normal';
+  chatClientId: string ='';
 
   constructor() {
 
     // Connect Socket with server URL
     this.socket = io('http://127.0.0.1:8000/test');
 
-    if (this.useType= "agent"){
-
-      console.log('New agent')
-      this.socket.emit('new_agent', {data: 'Agent connected to the SocketServer...'});
-    }
-    else{
-      console.log('New client')
-      this.socket.emit('new_client', {data: 'Client connected to the SocketServer...'});
-
-    }
-
-
+ 
 
    }
+
+   connectAgent(){
+
+    console.log('New agent')
+    this.socket.emit('new_agent', {data: 'Agent connected to the SocketServer...'});
+    this.userType ="agent";
+   
+
+   }
+
+   connectClient(clientId:string){
+
+      console.log('New client')
+      this.socket.emit('new_client', {data: 'Client connected to the SocketServer...','client_id':clientId});
+      this.userType ="client";
+      
+   
+   }
+
+   getAgentSupport(chat_text: string){
+    
+
+    this.socket.emit('need_support', {data: chat_text,client_id:this.chatClientId});
+
+
+
+  }
+
+
+  disconnectClient(clientId: string){
+
+
+    this.socket.emit('disconnect_private_conversation', {data: 'Client disconnected from conversation...','client_id':clientId});
+ 
+ }
 }
